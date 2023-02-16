72174a560d0c5e6779cfec072675b19d8cdfaa92
송한솔 <skycriperd@gmail.com>
2023-02-16 15:02:22 +0900

    02/16 15:00

---
+++ css/js/channel.js
@@ -0,0 +1,63 @@
+// 채널개설 //완성안된듯?
+function createChannel(){
+	var settings = {
+		"url": "http://localhost:8080/api/channels",
+		"method": "POST",
+		"timeout": 0,
+		"headers": {
+      		"Content-Type": "application/json",
+			"Authorization": localStorage.getItem('accessToken'),
+		},
+    "data": JSON.stringify({
+      "channelName": $('.createchannelname').val()
+    })
+	  };
+	$.ajax(settings).done(function (response) {
+	console.log(response);
+	location.reload()
+  });
+}
+
+
+// 전체 채널 조회
+function getChannelList(){
+	var settings = {
+		"url": "http://localhost:8080/api/users/channels",
+		"method": "GET",
+		"timeout": 0,
+		"headers": {
+      		"Content-Type": "application/json",
+			"Authorization": localStorage.getItem('accessToken'),
+			},
+		};
+		$.ajax(settings).done(function (response) {
+		console.log(response);
+		var html = '';
+		for (var i = 0; i < response.length; i++) {	
+		  if(response[i].userId != loginuserid) {
+			html += '<li>';
+			html += '<p class="channelsharp">'+'#'+'</p>';
+			html += '<p class="channelname">' + response[i].channelName + '</p>';
+			html += '</li>';
+		  }
+		}
+		document.querySelector('.chanelbox').innerHTML += html;
+	});
+}
+
+
+// 채널 검색// 채널 눌럿을때 채팅방에 로그인하기위함
+function SearchChannelList(){
+	var settings = {
+		"url": "http://localhost:8080/api/channels/"+채널아이디,
+		"method": "GET",
+		"timeout": 0,
+		"headers": {
+      		"Content-Type": "application/json",
+			"Authorization": localStorage.getItem('accessToken'),
+			}
+		};
+		$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
---
+++ css/js/component.js
@@ -0,0 +1,32 @@
+class MyComponent extends HTMLElement {
+  connectedCallback() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <style>
+          :host .personalmenu-popup button {
+            width:100%;
+            font-size: 16px;
+            border-radius: 4px;
+          }
+
+          :host .personalmenu-popup{
+            font-size: 16px;
+          }
+        
+        </style>
+        <div class="personalmenu-popup">
+          <h2 class="title">개인메뉴</h2>
+          <button onclick="location.href='updatemember.html'">회원정보수정</button>
+          <button onclick="deleteMember()">회원탈퇴</button>
+          <button onclick="logoutMember()">로그아웃</button>
+        </div>
+
+      
+    `;
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('my-component', MyComponent);
